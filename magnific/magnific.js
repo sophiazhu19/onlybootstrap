8b3ce2bc2e785ad4157a354523cc6dd3f5e0e2c0
Sophia <zhuz19wad@gmail.com>
2018-03-22 17:05:28 +1300

    update new pages

---
+++ magnific/magnific.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.img-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
